e5b88676fa9b2f9d17222de392c7f80c16ac18ea
Nathan Rajlich <nathan@tootallnate.net>
2017-12-07 13:41:47 -0800

    better prepend path

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
+const url = require('url');
+const http = require('http');
 const httpProxy = require('http-proxy');
+const target = process.argv[2];
+const parsed = url.parse(target);
+
 const config = {
-  target: process.env.PROXY_URL
+  target,
+  changeOrigin: true,
+  prependPath: true
 };
-httpProxy.createProxyServer(config).listen(8888);
+
+const proxy = httpProxy.createProxyServer();
+
+http.createServer((req, res) => {
+  if (req.url.startsWith(parsed.pathname)) {
+    req.url = req.url.substr(parsed.pathname.length) || '/';
+  }
+  proxy.web(req, res, config);
+}).listen(8888);
